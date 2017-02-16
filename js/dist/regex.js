6d1208ae4124be702df1eccda4d2e3682bdfc27b
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:37 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/regex.js
+++ js/dist/regex.js
@@ -13,10 +13,10 @@ if ( typeof exports === "object" ) {
 	definition( exports ) ;
 }
 else if ( typeof define === "function" && define.amd ) {
-	define( "aureooms-js-regex" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+	define( "@aureooms/js-regex" , [ ] , function ( ) { return definition( { } ) ; } ) ;
 }
 else if ( typeof window === "object" && typeof window.document === "object" ) {
 	definition( window["regex"] = { } ) ;
 }
-else console.error( "unable to detect type of module to define for aureooms-js-regex") ;
+else console.error( "unable to detect type of module to define for @aureooms/js-regex") ;
 } )( ) ;
